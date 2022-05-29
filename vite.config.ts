b93ddf05ea0b3e996aef93a9ffae3f91bd3bc5bb
harniy <forbedlam@gmail.com>
2022-05-29 13:13:45 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/roulette/',
+  base: '/',
   plugins: [vue(), Components({ /* options */ }),],
   resolve: {
     alias: {
